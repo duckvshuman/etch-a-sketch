c8b53058ec8b25acde20f12058f7cc67b67b9fbb
DvH <88995192+DangVietH@users.noreply.github.com>
2023-08-30 20:52:17 +0700

    add change size feature

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const drawing = document.querySelector(".drawing")
 
-bloxsize = 16
 
+createGrid(17)
 
-for (let x=0; x < bloxsize; x++) {
-    let gridline = document.createElement('div')
-    gridline.classList.add('gridbox-line')
-
-    // add box
-    for (let y=0; y < bloxsize; y++) {
-        let gridbox = document.createElement('div')
-        gridbox.classList.add('gridbox')
-        gridbox.addEventListener('mouseover', function (e) {e.target.style.background = 'black'})
-        gridline.appendChild(gridbox)
+function createGrid(boxAmount) {
+    drawing.replaceChildren()
+    for (let x=0; x < boxAmount; x++) {
+        let gridline = document.createElement('div')
+        gridline.classList.add('gridbox-line')
+    
+        // add box
+        for (let y=0; y < boxAmount; y++) {
+            let gridbox = document.createElement('div')
+            gridbox.classList.add('gridbox')
+            gridbox.addEventListener('mouseover', function (e) {
+                e.target.style.background = 'black'
+            })
+            gridline.appendChild(gridbox)
+        }
+    
+        drawing.appendChild(gridline)
     }
+}
 
-    drawing.appendChild(gridline)
+function changeSize() {
+    let value = prompt("How many squares do you want?", 16)
+    if (!Number(value)) {
+        alert("Input must be a number")
+    }
+    else if (value >= 100) {
+        alert("You can't enter 100 or more")
+    } else {
+        
+        createGrid(value)
+    }
 }
